82f37605bbf624dbbf0bcfa1dd0895b25e38f62b
sanchayyadav <59918633+sanchayyadav@users.noreply.github.com>
2022-04-14 11:28:07 +0530

    Update FlightRoutesRepository.java

--- airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRoutesRepository.java
+++ airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRoutesRepository.java
@@ -1,9 +1,20 @@
 package com.naehas.airlineassignment.crud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.naehas.airlineassignment.entity.FlightRoutes;
 
 public interface FlightRoutesRepository extends JpaRepository<FlightRoutes, Integer> {
+
+	@Query("SELECT flightRoutes FROM FlightRoutes flightRoutes ORDER BY flightRoutes.departOn ")
+
+	public List<FlightRoutes> findAllByOrderByDepartsOnAsc();
+	
+	@Query("SELECT flightRoutes FROM FlightRoutes flightRoutes ORDER BY flightRoutes.departOn DESC")
+
+	public List<FlightRoutes> findAllByOrderByDepartsOnDesc();
 
 }
