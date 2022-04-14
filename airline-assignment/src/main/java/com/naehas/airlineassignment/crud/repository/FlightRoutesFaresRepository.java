7f42d29faddb5078a5dbe982d4fc5efe0c5f656d
sanchayyadav <59918633+sanchayyadav@users.noreply.github.com>
2022-04-14 23:04:57 +0530

    Update FlightRoutesFaresRepository.java

--- airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRoutesFaresRepository.java
+++ airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRoutesFaresRepository.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.naehas.airlineassignment.entity.FlightRoutesFares;
 
 public interface FlightRoutesFaresRepository extends JpaRepository<FlightRoutesFares, Integer> {
 
-	@Query("SELECT flightRoutesFares FROM FlightRoutesFares flightRoutesFares ORDER BY flightRoutesFares.fare ")
-	public List<FlightRoutesFares> findAllByOrderByFareAsc();//doubt
+	public List<FlightRoutesFares> findAllByOrderByFareAsc();
 	
-	@Query("SELECT flightRoutesFares FROM FlightRoutesFares flightRoutesFares ORDER BY flightRoutesFares.fare DESC")
 	public List<FlightRoutesFares> findAllByOrderByFareDesc();
+	
+	@Query("SELECT flightRoutesFares FROM FlightRoutesFares flightRoutesFares WHERE flightRoutesFares.fare <= :fare")
+	public List<FlightRoutesFares> fareUpto(@Param("fare") int fare);
+	
+	@Query("SELECT flightRoutesFares FROM FlightRoutesFares flightRoutesFares WHERE flightRoutesFares.isRefundable = :refund")
+	public List<FlightRoutesFares> fareRefundable(@Param("refund") boolean refund );
 }
