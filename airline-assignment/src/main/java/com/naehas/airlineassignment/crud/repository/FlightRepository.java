c58fe2d1b3dcdb19c6ecf72f370ebad2b5f6e796
sanchayyadav <59918633+sanchayyadav@users.noreply.github.com>
2022-04-14 23:04:12 +0530

    Update FlightRepository.java

--- airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRepository.java
+++ airline-assignment/src/main/java/com/naehas/airlineassignment/crud/repository/FlightRepository.java
@@ -1,12 +1,15 @@
 package com.naehas.airlineassignment.crud.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.naehas.airlineassignment.entity.Flight;
-
+import com.naehas.airlineassignment.entity.FlightRoutesFares;
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
-    public List<FlightRoutesFares> findAllByOrderByIdAsc();
-    public List<FlightRoutesFares> findAllByOrderByIdDesc();
-  	public List<FlightRoutes> findAllByOrderdepartsOnAsc();
-
+   
+	@Query("SELECT flight FROM Flight flight WHERE flight.airline LIKE %?1%")
+	public List<Flight> findAirline(String airline);
+	
 }
